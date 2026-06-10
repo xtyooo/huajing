a85c0627d05a0161c6aa994a4bda3d01dc42930e
qiu <609799548@qq.com>
2026-06-10 21:54:35 +0800

    feat: add media router

---
+++ router/media-router.go
@@ -0,0 +1,18 @@
+package router
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/QuantumNous/new-api/common"
+	"github.com/gin-gonic/gin"
+)
+
+func SetMediaRouter(router *gin.Engine) {
+	mediaDir := common.GetEnvOrDefaultString("MEDIA_DIR", "/media")
+	if info, err := os.Stat(mediaDir); err != nil || !info.IsDir() {
+		common.SysLog(fmt.Sprintf("MEDIA_DIR %s does not exist or is not a directory", mediaDir))
+		return
+	}
+	router.Static("/media", mediaDir)
+}
